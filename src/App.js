421630f223711d553160a7f0e2bfc34f260bbfc0
kyllihsm <kullihaasma@gmail.com>
2018-12-13 19:53:09 +0200

    xmas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <img src={process.env.PUBLIC_URL + '/esimene_pic.png'} className="esimene-pic" alt="logo" />
+       <audio src={process.env.PUBLIC_URL + '/wham.mp3'} autoPlay>
+
+       </audio>
+          <div className="text-yo">
+            <h1>PIPEDRIVE CHRISTMAS PARTY VOL FAMILY REUNION</h1>
+            <h2>TOMORROW AT 7PM</h2>
+            <h3>DRESSCODE: UGLY SWEATER</h3>
+            <h4>FOOTWEAR: INDOOR SHOES</h4>
+            <h5>IF YOU NEED SMTH FROM YOUR ROOM ON SATURDAY THEN GET IT NOW SINCE YOU WON'T BE ABLE TO ACCESS OTHER FLOORS DURING THE PARTY</h5>
+            <h6>#WHAMAGEDDON</h6>
+          </div>
       </div>
     );
   }
